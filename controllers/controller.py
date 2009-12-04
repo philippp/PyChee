754f58433ac0ff5f0c0db823c373c13e592a3aca
philippp <philippp@notphil.(none)>
2009-12-04 06:49:40 +0000

    Move base controller into dedicated module

---
+++ controllers/controller.py
@@ -0,0 +1,32 @@
+class Controller(object):
+    
+    def render(self, **kwargs):
+        target = kwargs.get('view',None)
+        if target:
+            del kwargs['view']
+        else:
+            target = self.__class__.__name__.lower()
+        
+        # We parse out periods to support nested modules within templates.
+        target = "views.%s" % target
+        components = target.split('.')
+        mod = __import__(target)
+        for comp in components[1:]:
+            mod = getattr(mod, comp)
+
+        # Cheetah guarantees that the template class is named the same as the template module.
+        tmpl = getattr(mod, components[-1])()
+        [setattr(tmpl, k, v) for k,v in kwargs.items()]
+        return str(tmpl)
+    
+    @classmethod
+    def dispatch(cls, req, uri, **kwargs):
+        ''' Routing dispatcher, triggers any and all controller activity.
+        /foo/bar will invoke method bar on controller foo.
+        '''
+        controller_obj = cls()
+        uri_parts = uri.split("/")
+        if uri_parts and len(uri_parts) > 1 and getattr(controller_obj, uri_parts[1],None):
+            return getattr(controller_obj, uri_parts[1])(**kwargs) 
+        else:
+            return controller_obj.index(**kwargs)
