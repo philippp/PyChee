b43bee915d0f06788a9938d9194ad29fbfa3a90a
philippp <philippp@notphil.(none)>
2009-12-05 21:58:46 +0000

    Added a per-category listing function

--- lib/diskio.py
+++ lib/diskio.py
@@ -3,21 +3,13 @@ Super simple object storage mechanism.
 Stores pickle-able data to the data directory and return an ID.
 
 TODO: Semaphores to avoid write contention over the same file
-
-TODO: Support indexing and search:
-        Define data subclass that generates a hash for indexable K-V pairs.
-        Include this hash in the name and use it for lookup / return.
 '''
 import os
 import cPickle
 
-C_LEN = 8 #Category Length
-
-def write(data, category=0):
-    
+def write(data, category='0'):    
     # need semaphore, get it / TODO
-    c = '0' * (C_LEN - len(str(category))) + str(category)
-    cdir = 'data/%s' % c
+    cdir = 'data/%s' % category
     if not os.access(cdir,os.R_OK): #Todo: Check to existence vs. perms
         os.mkdir(cdir)
     id = len(os.listdir(cdir)) + 1    
@@ -27,6 +19,13 @@ def write(data, category=0):
     # release semaphore
     return id
 
-def read(id, category=0):
+def read(id, category='0'):
     f = open('data/%s/%s' % (category, id), 'r')
     return cPickle.load(f)
+
+def list(category='0'):
+    cdir = 'data/%s' % category
+    if not os.access(cdir,os.R_OK):
+        return []
+    ids = os.listdir(cdir)
+    return dict( [ (id, read(id)) for id in ids ] )
