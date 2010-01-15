c45e02e19e0e7db1bca00761dea0634cc1e82e18
philippp <philippp@notphil.(none)>
2010-01-15 08:16:09 +0000

    Added application directory lookup to main config

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-data_dir = '/home/philippp/pychee/data'
+root_dir = '/home/philippp/pychee'
+data_dir = '%s/data' % root_dir
 diskio_dir = data_dir + '/diskio'
 memcache_host = '127.0.0.1:11211'
-log_dir = '/home/philippp/pychee/logs'
+log_dir = '%s/logs' % root_dir
+app_dir_for = lambda _app: '%s/app/%s' % (root_dir, _app)
