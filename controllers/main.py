f7a6602e0a23cc53d1cfb1f40355198bb9ac67c8
Philipp Pfeiffenberger <git@notphil.com>
2010-05-09 18:40:42 -0700

    Brought top blogspot article to the front page

--- controllers/main.py
+++ controllers/main.py
@@ -1,8 +1,29 @@
 import os
+import lib.RSS
+import rfc822
 from lib import controller
 
 class main(controller.Controller):
 
   def index(self):
-    return self.render("main")
+    stories = self.get_stories()
+    return self.render("main",
+                       stories = stories)
     
+  def get_stories(self):
+    c = lib.RSS.TrackingChannel()
+    c.parse("http://100bedsforhaitiblog.blogspot.com/feeds/posts/default?alt=rss")
+    news_items = []
+    guids = []
+    for item in c.items():
+      news_item = {}
+      try:
+        for k, v in item[1][0].iteritems():
+          news_item[k[1]] = v
+        news_items.append(news_item)
+      except Exception:
+        pass
+    news_items = sorted(news_items,
+                        key = lambda s : rfc822.mktime_tz(rfc822.parsedate_tz(s['pubDate'])),
+                        reverse=True)
+    return news_items
